e14e3437ac02a367032ce655f73656aed629954d
jefshandler <jef_shandler@msn.com>
2022-08-22 16:37:11 -0300

    Add project files.

---
+++ C-webScraper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_webScraper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. get all links
+            // 2. porocess al links - book Detail
+            // 3. export csv file
+            // get the page
+            // get a object - parsing
+            // querying 
+            // CSV
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
